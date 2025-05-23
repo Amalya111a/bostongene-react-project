a031024ca2ca7a92d06ee151faeb318d864204e2
Higard <andoxost666@gmail.com>
2025-05-23 15:45:26 +0400

    test

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,34 +1,64 @@
-// import React from 'react';
+// import styles from "./Login.module.scss";
+// import React from "react";
+// import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+// import { Button, Input, Space } from "antd";
 
-// const Login = () => {
-//   return <h1>📅 Login page</h1>;
-// };
-
-// export default Login;
+// export default function Login() {
+//   return (
+//     <div className={styles.wrapper}>
+      
+//         <div className={styles.item}>
+//             <span>Login:</span>
+//           <Input placeholder="Input Username" />
+//         </div>
+//         <div className={styles.item}>
+//           {" "}
+//           <span>Password:</span>
+//           <Input.Password
+//             placeholder="input password"
+//             iconRender={(visible) =>
+//               visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+//             }
+//           />
+//         </div>
+      
+//     </div>
+//   );
+// }
 import styles from "./Login.module.scss";
 import React from "react";
 import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
-import { Button, Input, Space } from "antd";
+import { Button, Input } from "antd";
+import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
     <div className={styles.wrapper}>
-      
-        <div className={styles.item}>
-            <span>Login:</span>
-          <Input placeholder="Input Username" />
+      <div className={styles.item}>
+        <span>Login:</span>
+        <Input placeholder="Input Username" />
+      </div>
+      <div className={styles.item}>
+        <span>Password:</span>
+        <Input.Password
+          placeholder="Input Password"
+          iconRender={(visible) =>
+            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+          }
+        />
+      </div>
+
+      <div className={styles.actions}>
+        <Button type="primary" block>
+          Log In
+        </Button>
+        <div className={styles.signup}>
+          <span>First time here?</span>{" "}
+          <Link to="/Regestration">
+            <Button type="link">Sign Up</Button>
+          </Link>
         </div>
-        <div className={styles.item}>
-          {" "}
-          <span>Password:</span>
-          <Input.Password
-            placeholder="input password"
-            iconRender={(visible) =>
-              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-            }
-          />
-        </div>
-      
+      </div>
     </div>
   );
 }
